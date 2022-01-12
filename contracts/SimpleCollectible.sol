a8af9245833b600487b1c3d673a7f1a09ea8f467
francisco <fr.betancourt@live.com>
2022-01-12 12:54:30 -0300

    ERC721 constructor called.

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -4,4 +4,6 @@ pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract SimpleCollectible is ERC721 {}
+contract SimpleCollectible is ERC721 {
+    constructor() public ERC721("Dogie", "DOG") {}
+}
