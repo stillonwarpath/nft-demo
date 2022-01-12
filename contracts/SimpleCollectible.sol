153372e7ad4bb030e6a1637c27c8742c04d2e6ab
francisco <fr.betancourt@live.com>
2022-01-12 12:58:14 -0300

    Added function definition for creating collectible.

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -6,4 +6,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
     constructor() public ERC721("Dogie", "DOG") {}
+
+    function createCollectible() public returns (uint256) {}
 }
