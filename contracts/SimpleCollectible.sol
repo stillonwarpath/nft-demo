cd87c250200d39123a522617e27989ce4e8548d3
francisco <fr.betancourt@live.com>
2022-01-13 16:14:09 -0300

    Set Token URI for associating NFT to image.

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -11,9 +11,13 @@ contract SimpleCollectible is ERC721 {
         tokenCounter = 0;
     }
 
-    function createCollectible() public returns (uint256) {
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
         uint256 newTokenId = tokenCounter;
         _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
         tokenCounter = tokenCounter + 1;
         return newTokenId;
     }
