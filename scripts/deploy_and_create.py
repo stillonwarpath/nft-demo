022d2529ca15c75f66bfd0fedff826071384d578
francisco <fr.betancourt@live.com>
2022-01-13 17:16:51 -0300

    Imported simple collectible contract from brownie. Added dotenv to brownie configuration for reading .env environment variables.

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,4 +1,6 @@
-from scripts.helpful_scripts import get_account, SimpleCollectible
+from brownie import SimpleCollectible
+from scripts.helpful_scripts import get_account
+
 
 sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
 OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
