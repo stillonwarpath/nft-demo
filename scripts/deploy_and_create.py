f0346c2a927c81ea8fc35e79781585a0b5470e4b
francisco <fr.betancourt@live.com>
2022-01-13 16:44:30 -0300

    Deployed Simple Collectible. Called function for creating collectible.

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,2 +1,11 @@
+from scripts.helpful_scripts import get_account, SimpleCollectible
+
+sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+OPENSEA_URL = "https://testnets.opensea.io/assets" 
+
 def main():
     account = get_account()
+    simple_collectible = SimpleCollectible.deploy({"from": account})
+    tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
+    tx.wait(1)
+    print(f"Awesome, you can view your NFT at {}")
