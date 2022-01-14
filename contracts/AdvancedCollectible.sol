a28eece94f9b482b26b7f5dcfb3dba28281862c6
francisco <fr.betancourt@live.com>
2022-01-14 13:28:25 -0300

    Defined constructor and initialized VRFConsumerBase and ERC721 constructors.

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -9,4 +9,15 @@ pragma solidity 0.6.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract AdvancedCollectible is ERC721 {}
+contract AdvancedCollectible is ERC721, VRFConsumerBase {
+    constructor(
+        address _vrfCoordinator,
+        address _linkToken,
+        bytes32 _keyHash,
+        uint256 fee
+    )
+        public
+        VRFConsumerBase(_vrfCoordinator, _linkToken)
+        ERC721("Dogie", "DOG")
+    {}
+}
