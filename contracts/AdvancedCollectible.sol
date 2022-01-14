16ce506eb4a33e34f27d237337433caab3106cb5
francisco <fr.betancourt@live.com>
2022-01-14 13:22:56 -0300

    Added chainlink VRF consumer base contract dependency.

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -7,5 +7,6 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721 {}
