62b0aa1c93d586f302034c7c8b4dca206d65d5e0
francisco <fr.betancourt@live.com>
2022-01-14 13:31:15 -0300

    Defined token counter, keyhash and fee attributes.

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -10,14 +10,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
+    uint256 public tokenCounter;
+    bytes32 public keyhash;
+    uint256 public fee;
+
     constructor(
         address _vrfCoordinator,
         address _linkToken,
         bytes32 _keyHash,
-        uint256 fee
+        uint256 _fee
     )
         public
         VRFConsumerBase(_vrfCoordinator, _linkToken)
         ERC721("Dogie", "DOG")
-    {}
+    {
+        tokenCounter = 0;
+        keyhash = _keyHash;
+        fee = _fee;
+    }
 }
