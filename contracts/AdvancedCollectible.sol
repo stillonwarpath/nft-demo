aaad42dcbe4bd175f0f65c1e0d9596953a2db538
francisco <fr.betancourt@live.com>
2022-01-14 13:20:04 -0300

    Added advanced collectible contract definition.

---
+++ contracts/AdvancedCollectible.sol
@@ -0,0 +1,11 @@
+// An NFT Contract
+// Where the tokenURI can be one of 3 different dogs
+// Randomly selected
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract AdvancedCollectible is ERC721 {}
