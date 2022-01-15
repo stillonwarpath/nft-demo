cea22066d7a8562af54ff87c0fda4ff53d585fa6
francisco <fr.betancourt@live.com>
2022-01-15 16:08:03 -0300

    Added function for getting contract, and function for deploying mocks.

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,4 @@
-from brownie import accounts, network, config
+from brownie import accounts, network, config, Contract
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache", "mainnet-fork"]
 OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
@@ -12,3 +12,39 @@ def get_account(index=None, id=None):
     if id:
         return accounts.load(id)
     return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_mock = {"vrf_coordinator": VRFCoordinatorMock, "link_token": LinkToken}
+
+
+def get_contract(contract_name):
+    """This function will grab the contract addresses from the brownie config
+    if defined, otherwise it will deploy a mock version of that contract, and
+    return that mock contract.
+        Args:
+            contract_name (string)
+        Returns:
+            brownie.network.contract.ProjectContract : The most recently deployed version
+            of this contract.
+            MockV3Aggregator[-1]
+    """
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        # address
+        # ABI
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    return contract
+
+
+def deploy_mocks():
+    account = get_account()
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print("Deployed!")
