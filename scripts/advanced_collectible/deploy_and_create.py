d390277e87fdfc2350328ab3bc713d3e644ab84b
francisco <fr.betancourt@live.com>
2022-01-15 16:35:03 -0300

    Passed parameters to advanced collectible contract initialization.

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -1,20 +1,19 @@
-from brownie import SimpleCollectible
-from scripts.helpful_scripts import get_account, OPENSEA_URL
-
-
-sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+from brownie import AdvancedCollectible, network, config
+from scripts.helpful_scripts import get_account, OPENSEA_URL, get_contract
 
 
 def deploy_and_create():
+    # We want to be able to use the deployed contract if we are on a testnet
+    # Otherwise, we weant to deploy some mocks and use those
+    # Rinkeby
     account = get_account()
-    simple_collectible = SimpleCollectible.deploy({"from": account})
-    tx = simple_collectible.createCollectible(sample_token_uri, {"from": account})
-    tx.wait(1)
-    print(
-        f"Awesome, you can view your NFT at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}"
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
+        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": account},
     )
-    print("Please wait up to 20 minutes, and hit the refresh metadata button.")
-    return simple_collectible
 
 
 def main():
