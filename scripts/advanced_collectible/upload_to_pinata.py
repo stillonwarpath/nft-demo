7207f737c5c019bc646601430438067ae3ef6486
francisco <fr.betancourt@live.com>
2022-01-19 11:58:44 -0300

    Send post requesr for uploading NFT file to Pinata

--- scripts/advanced_collectible/upload_to_pinata.py
+++ scripts/advanced_collectible/upload_to_pinata.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import requests
 
 PINATA_BASE_URL = "https://api.pinata.cloud/"
 endpoint = "pinning/pinFileToIPFS"
@@ -11,5 +12,13 @@ headers = {
     "pinata_secret_api_key": os.getenv("PINATA_API_SECRET"),
 }
 
-with Path(filepath).open("rb") as fp:
-    image_binary = fp.read()
+
+def main():
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        response = requests.post(
+            PINATA_BASE_URL + endpoint,
+            files={"file": (filename, image_binary)},
+            headers=headers,
+        )
+        print(response.json())
