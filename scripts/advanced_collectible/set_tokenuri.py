3cbdcec971786986883b68703dc2eecf3bf449ca
francisco <fr.betancourt@live.com>
2022-01-20 14:36:29 -0300

    Check if advanced collectible owne the tokenURI.

---
+++ scripts/advanced_collectible/set_tokenuri.py
@@ -0,0 +1,13 @@
+from brownie import network, AdvancedCollectible
+from scripts.helpful_scripts import get_breed
+
+
+def main():
+    print(f"Working on {network.show_active()}")
+    advanced_collectible = AdvancedCollectible[-1]
+    number_of_collectibles = advanced_collectible.tokenCounter()
+    print(f"You have {number_of_collectibles} tokenIds")
+    for token_id in range(number_of_collectibles):
+        breed = get_breed(advanced_collectible.tokenIdToBreed(token_id))
+        if not advanced_collectible.tokenURI(token_id).startswith("https://"):
+            print(f"Setting tokenURI of {token_id}")
