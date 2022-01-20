b1442ed7441f8d318faac07d23293233ef139947
francisco <fr.betancourt@live.com>
2022-01-20 15:33:17 -0300

    Pass parameters to set_tokenURI function.

--- scripts/advanced_collectible/set_tokenuri.py
+++ scripts/advanced_collectible/set_tokenuri.py
@@ -1,6 +1,12 @@
 from brownie import network, AdvancedCollectible
 from scripts.helpful_scripts import OPENSEA_URL, get_account, get_breed
 
+dog_metadata_dic = {
+    "PUG": "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json",
+    "SHIBA_INU": "https://ipfs.io/ipfs/QmdryoExpgEQQQgJPoruwGJyZmz6SqV4FRTX1i73CT3iXn?filename=1-SHIBA_INU.json",
+    "ST_BERNARD": "https://ipfs.io/ipfs/QmbBnUjyHHN7Ytq9xDsYF9sucZdDJLRkWz7vnZfrjMXMxs?filename=2-ST_BERNARD.json",
+}
+
 
 def main():
     print(f"Working on {network.show_active()}")
@@ -11,7 +17,7 @@ def main():
         breed = get_breed(advanced_collectible.tokenIdToBreed(token_id))
         if not advanced_collectible.tokenURI(token_id).startswith("https://"):
             print(f"Setting tokenURI of {token_id}")
-            set_tokenURI()
+            set_tokenURI(token_id, advanced_collectible, dog_metadata_dic[breed])
 
 
 def set_tokenURI(token_id, nft_contract, tokenURI):
