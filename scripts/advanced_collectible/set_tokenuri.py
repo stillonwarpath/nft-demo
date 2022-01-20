d7acd76e85bd86ceb2fb5a8b5fb4b7038e971921
francisco <fr.betancourt@live.com>
2022-01-20 14:41:39 -0300

    Defined function for setting tokenURI of all advanced collectibles.

--- scripts/advanced_collectible/set_tokenuri.py
+++ scripts/advanced_collectible/set_tokenuri.py
@@ -1,5 +1,5 @@
 from brownie import network, AdvancedCollectible
-from scripts.helpful_scripts import get_breed
+from scripts.helpful_scripts import OPENSEA_URL, get_account, get_breed
 
 
 def main():
@@ -11,3 +11,14 @@ def main():
         breed = get_breed(advanced_collectible.tokenIdToBreed(token_id))
         if not advanced_collectible.tokenURI(token_id).startswith("https://"):
             print(f"Setting tokenURI of {token_id}")
+            set_tokenURI()
+
+
+def set_tokenURI(token_id, nft_contract, tokenURI):
+    account = get_account()
+    tx = nft_contract.setTokenURI(token_id, tokenURI, {"from": account})
+    tx.wait(1)
+    print(
+        f"Awesome! You can view your NFT at {OPENSEA_URL.format(nft_contract.address, token_id)}"
+    )
+    print("Please wait up to 20 minutes, and hit refresh metada button")
